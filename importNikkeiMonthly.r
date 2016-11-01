7f88a55f3f2e1e6ca92b04a74017718faf7b84c6
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2016-11-01 16:53:40 +0900

    日経平均株価取得コード新規アップロード
    日経平均株価取得コード新規アップロード

---
+++ importNikkeiMonthly.r
@@ -0,0 +1,8 @@
+urlToFile <- 'http://indexes.nikkei.co.jp/nkave/historical/nikkei_stock_average_monthly_jp.csv'
+tmp0 <-
+  read.csv(urlToFile, header = T, skip = 0, stringsAsFactor = F, na.strings = c("", "***"), check.names = F, fileEncoding = "cp932")
+tmp1 <- na.omit(tmp0)
+tmp1[,1] <- as.Date(tmp1[,1])
+colnames(tmp1)[-1] <- paste0('日経平均株価-',colnames(tmp1)[-1])
+colnames(tmp1)[1] <- 'Date'
+assign('nikkeiMonthly', tmp1, envir = .GlobalEnv)
