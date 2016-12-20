e94c61bef6695c7d3ad804049c5068aebf1893c9
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2016-12-20 12:45:20 +0900

    MONTHLY STATEMENT OF THE PUBLIC DEBT取得コード新規アップロード
    MONTHLY STATEMENT OF THE PUBLIC DEBT OF THE UNITED STATES取得コード新規アップロード

---
+++ importMonthlyStatementOfThePublicDebtOfTheUnitedStates.r
@@ -0,0 +1,32 @@
+fun_MSPD <- function(startYear = 2013, breakYear =2016, breakMonth = 9999){
+  library(XLConnect);library(Nippon);library(lubridate);library(excel.link)
+  username <- Sys.info()['user']
+  pathOutput <- paste0("C:/Users/", username, "/Desktop/MSPD/")
+  setwd(pathOutput)
+  MSPD <- data.frame()
+  cnt <- 0
+  for(yyyy in startYear:breakYear){
+    for(mm in 1:12){
+      if(yyyy == breakYear & mm == breakMonth){break}
+      dataDate <- paste0(yyyy,'-',mm,'-1')
+      fileName <- paste0('opdm',formatC(mm,width = 2,flag = '0'),yyyy,'.xls')
+      if(!file.exists(paste0(pathOutput, fileName))) {
+        download.file(paste0('https://www.treasurydirect.gov/govt/reports/pd/mspd/',yyyy,'/',fileName),
+                      fileName, mode = "wb")
+      }
+      buf0 <-
+        readWorksheetFromFile(paste0(pathOutput, fileName), sheet = 1, check.names = F, header = F)
+      buf0[sapply(buf0,function(x)regexpr('\\w',x)) == -1] <- NA
+      buf1 <- buf0[,apply(buf0,2,function(x){sum(is.na(x))}) != nrow(buf0)]
+      buf2 <- buf1[apply(buf1,1,function(x){sum(is.na(x))}) != ncol(buf1),]
+      cnt <- cnt + 1
+      MSPD[cnt,1] <- dataDate
+      MSPD[cnt,2] <-
+        as.numeric(gsub(',','',buf2[grep('Total Public Debt Outstanding',buf2[,1],ignore.case = T),ncol(buf2)]))
+    }
+  }
+  MSPD[,1] <- as.Date(MSPD[,1])
+  colnames(MSPD) <- c('Date','Total Public Debt Outstanding(Millions of dollars)')
+  assign('MSPD',MSPD,envir = .GlobalEnv)
+  assign('tableTitle', 'MONTHLY STATEMENT OF THE PUBLIC DEBT OF THE UNITED STATES', envir = .GlobalEnv)
+}
