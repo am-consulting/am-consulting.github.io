664ee5be3b2700a25444c7c293f6fe48461e6fab
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2016-12-26 12:29:57 +0900

    最後の転換から直近までの期間追記、0除外の場合の修正
    最後の転換から直近までの期間追記、0除外の場合の修正

--- R-turningPoint.r
+++ R-turningPoint.r
@@ -1,11 +1,15 @@
 fun_turningPoint <- function(obj, dateFormat = '%Y-%m'){
-  obj[,1] <-  format(obj[,1],dateFormat)
+  obj[,1] <- as.Date(obj[,1])
   obj0 <- obj
   obj0[obj0 == 0] <- 1
-  assign('objPNinclude0',
-         obj[which(head(obj0[,2],-1) * tail(obj0[,2],-1) < 0) + 1,], envir = .GlobalEnv)
+  obj0 <- obj[which(head(obj0[,2],-1) * tail(obj0[,2],-1) < 0) + 1,]
+  assign('durationInclude0', nrow(subset(obj, tail(obj0[,1],1) <= obj[,1])))
+  obj0[,1] <-  format(obj0[,1],dateFormat)
+  assign('objPNinclude0', obj0, envir = .GlobalEnv)
   obj0 <- obj
   obj0 <- subset(obj0,obj0[,2] != 0)
-  assign('objPNexclude0',
-         obj[which(head(obj0[,2],-1) * tail(obj0[,2],-1) < 0) + 1,], envir = .GlobalEnv)
+  obj0 <- obj0[which(head(obj0[,2],-1) * tail(obj0[,2],-1) < 0) + 1,]
+  assign('durationExclude0', nrow(subset(obj, tail(obj0[,1],1) <= obj[,1])))
+  obj0[,1] <-  format(obj0[,1],dateFormat)
+  assign('objPNexclude0', obj0, envir = .GlobalEnv)
 }
