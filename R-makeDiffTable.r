8dbe4fdb08df03c79e020f3399d2424090ac9539
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-01-04 11:56:03 +0900

    差分データフレーム作成関数新規アップロード
    差分データフレーム作成関数新規アップロード

---
+++ R-makeDiffTable.r
@@ -0,0 +1,9 @@
+fun_makeDiffTable <-
+  function(obj, lag = 1, diff = 1){
+    diffDF <-
+      data.frame(tail(obj[,1],-1),
+                 diff(as.matrix(obj[,-1]), lag = lag, differences = diff),
+                 check.names = F,
+                 stringsAsFactors = F)
+    colnames(diffDF) <- colnames(obj)
+  }
