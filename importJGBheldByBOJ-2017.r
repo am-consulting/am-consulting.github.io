3770c181eca01911f1d9a96392067de221d6aa49
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-01-05 19:25:36 +0900

    buf0[is.na(buf)]<-""を削除して列名をgsubで処理。NAセルが意味を持ってしまうため削除。
    buf0[is.na(buf)]<-""を削除して列名をgsubで処理。NAセルが意味を持ってしまうため削除。

--- importJGBheldByBOJ-2017.r
+++ importJGBheldByBOJ-2017.r
@@ -18,16 +18,30 @@ download.file(paste0('https://www.boj.or.jp', xlsxList[1]),
               mode = 'wb')
 buf0 <-
   readWorksheetFromFile(paste0(pathOutput, fileName), sheet = 1, check.names = F, header = F)
-title <- paste0(buf0[3,3],buf0[4,3])
-dfR  <- which(apply(buf0,1,function(x)length(grep('銘柄',x)))!=0)[2]
-dfC1 <- which(apply(buf0,2,function(x)length(grep('銘柄',x)))!=0)[1]
-dfC2 <- which(apply(buf0,2,function(x)length(grep('保有残高',x)))!=0)[1]
-buf0[is.na(buf0)] <- ''
-buf0[dfR,] <- paste0(buf0[dfR,],buf0[dfR-1,])
-buf <- buf0[dfR:nrow(buf0),dfC1:dfC2]
-colnames(buf) <- buf[1,]
-buf <- buf[-1,]
-buf[,3] <- as.numeric(gsub(',','',buf[,3]))
-buf <- na.omit(buf)
-colnames(buf)[2] <- 'x回債'
-assign('JGBheldByBOJ', buf, envir = .GlobalEnv)
+title <-
+  paste0(buf0[3,3],buf0[4,3])
+dfR  <-
+  which(apply(buf0,1,function(x)length(grep('銘柄',x)))!=0)[2]
+dfC1 <-
+  which(apply(buf0,2,function(x)length(grep('銘柄',x)))!=0)[1]
+dfC2 <-
+  which(apply(buf0,2,function(x)length(grep('保有残高',x)))!=0)[1]
+buf0[dfR,] <-
+  paste0(buf0[dfR,],buf0[dfR-1,])
+buf <-
+  buf0[dfR:nrow(buf0),dfC1:dfC2]
+colnames(buf) <-
+  buf[1,]
+buf <-
+  buf[-1,]
+buf[,3] <-
+  as.numeric(gsub(',','',buf[,3]))
+buf <-
+  na.omit(buf)
+colnames(buf) <-
+  gsub('NA','',colnames(buf))
+colnames(buf)[2] <-
+  'x回債'
+assign('JGBheldByBOJ',
+       buf,
+       envir = .GlobalEnv)
