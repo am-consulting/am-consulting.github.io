497f5b46646147f5529647d5216852e305fd4256
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-01-06 12:59:24 +0900

    全面的改善
    全面的改善

--- R-getLatestResult.r
+++ R-getLatestResult.r
@@ -1,5 +1,14 @@
-fun_latestResult <- function(obj, objColumn = -1){
-  assign('latestResult',
-         paste0(paste0(colnames(obj)[objColumn], ':', tail(obj[,objColumn],1)), collapse = ','),
-         envir = .GlobalEnv)
+fun_latestResult <-
+  function(obj, objColumn = 2, orderDirection = 0, orderColumn = 1, dateColumn = 1, dateFormat = '%Y-%m'){
+    if(orderDirection == 1){
+    obj <-
+      obj[order(obj[,orderColumn], decreasing = T),]
+    }
+    assign('latestResult',
+           paste0(ifelse(dateFormat == 0, tail(obj[,dateColumn],1), format(tail(obj[,dateColumn],1),dateFormat)),
+                  ':',
+                  colnames(obj)[objColumn],
+                  ':',
+                  tail(obj[,objColumn],1)),
+           envir = .GlobalEnv)
 }
