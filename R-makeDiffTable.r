f95ab3759cadccb31240fc3fc21f5fef0bdc6143
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-01-09 21:07:10 +0900

    日付列のtail修正、四捨五入追記、比を取る関数の追記
    日付列のtail修正、四捨五入追記、比を取る関数の追記

--- R-makeDiffTable.r
+++ R-makeDiffTable.r
@@ -1,10 +1,21 @@
 fun_makeDiffTable <-
-  function(obj, lag = 1, diff = 1){
+  function(obj, lag = 1, diff = 1, roundDigits = 10){
     diffDF <-
-      data.frame(tail(obj[,1],-1),
-                 diff(as.matrix(obj[,-1]), lag = lag, differences = diff),
+      data.frame(tail(obj[,1],- lag * diff),
+                 round(diff(as.matrix(obj[,-1]), lag = lag, differences = diff), roundDigits),
                  check.names = F,
                  stringsAsFactors = F)
     colnames(diffDF) <- colnames(obj)
     assign('diffDF', diffDF, envir = .GlobalEnv)
+  }
+
+fun_makeDiffRatioTable <-
+  function(obj, lag = 1, diff = 1, roundDigits = 1){
+    diffRatioDF <-
+      data.frame(tail(obj[,1],- lag * diff),
+                 round(diff(as.matrix(obj[,-1]), lag = lag, differences = diff)/head(obj[,-1], - lag * diff) * 100, roundDigits),
+                 check.names = F,
+                 stringsAsFactors = F)
+    colnames(diffRatioDF) <- colnames(obj)
+    assign('diffRatioDF', diffRatioDF, envir = .GlobalEnv)
   }
