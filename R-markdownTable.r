95dae5ccc1b39fd08bafad6d500059dfc9a53cad
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-01-12 13:05:54 +0900

    table.attr追記
    table.attr追記

--- R-markdownTable.r
+++ R-markdownTable.r
@@ -1,7 +1,8 @@
-fun_markdownTable <- function(obj, rownames = F){
+fun_markdownTable <- function(obj, rownames = F, table.attr = "id = 'amcc' width = '100%'"){
   knitr::kable(obj,
                row.names = rownames,
                align = 'r',
                format = 'markdown',
-               format.args = list(big.mark = ',', drop0trailing = T))
+               format.args = list(big.mark = ',', drop0trailing = T),
+               table.attr = table.attr)
 }
