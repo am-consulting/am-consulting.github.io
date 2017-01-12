ed5c27687305b34ef7fa9b50fb3fd7acd782c522
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-01-13 00:45:37 +0900

    対外・対内 直接投資収益取得コード完成
    対外・対内 直接投資収益取得コード完成

--- importDirectInvestmentByRegionAndIndustry.r
+++ importDirectInvestmentByRegionAndIndustry.r
@@ -1,4 +1,6 @@
 library(XLConnect);library(Nippon)
+# http://stackoverflow.com/questions/24534782/how-do-skip-or-f-work-on-regex
+# http://stackoverflow.com/questions/8613237/extract-info-inside-all-parenthesis-in-r
 username <-
   Sys.info()['user']
 pathOutput <-
@@ -8,21 +10,45 @@ fileURL <-
   'http://www.boj.or.jp/statistics/br/bop_06/dii163q.xlsx'
 fileName <-
   'DirectInvestmentByRegionAndIndustry.xlsx'
-download.file(fileURL, fileName, mode = 'wb')
+# download.file(fileURL, fileName, mode = 'wb')
+sheetTitle <- vector()
 for(sheetNo in 1:4){
-  assign(paste0('DirectInvestmentByRegionAndIndustry',sheetNo),
-         readWorksheetFromFile(paste0(pathOutput, fileName), sheet = sheetNo, check.names = F, header = F))
+  buf0 <-
+    readWorksheetFromFile(paste0(pathOutput, fileName), sheet = sheetNo, check.names = F, header = F)
+  buf1 <-
+    buf0
+  sheetTitle[sheetNo] <-
+    zen2han(buf1[8,3])
+  colnames(buf1) <-
+    gsub('\\s|na','',sapply(paste0(buf1[11,],buf1[12,],buf1[13,]),zen2han),ignore.case = T)
+  buf1[,1] <-
+    gsub('na','',sapply(paste0(buf1[,1],buf1[,2]),zen2han),ignore.case = T)
+  buf2 <-
+    buf1[buf1[,1]!='',]
+  buf3 <-
+    buf2[,c(1,which(colnames(buf2)!=''))]
+  buf4 <-
+    buf3[!is.na(buf3[,2]),]
+  buf5 <-
+    apply(buf4,2,function(x)as.numeric(gsub("[\\(\\)]", "",
+                                            regmatches(x,gregexpr("\\([0-9]*?\\)",x))))*-1)
+  buf5[is.na(buf5)] <-
+    buf4[is.na(buf5)]
+  buf6 <-
+    data.frame(ID = seq(nrow(buf5)),
+               buf5[,1,drop=F],
+               as.matrix(apply(buf5[,-1],2,function(x)as.numeric(gsub(',','',x)))),
+               check.names = F,
+               stringsAsFactors = F,
+               row.names = NULL)
+  colnames(buf6)[2] <- '地域'
+  assign(paste0('DirectInvestment',sheetNo),
+         buf6,
+         envir = .GlobalEnv)
 }
-dataSet <- DirectInvestmentByRegionAndIndustry1
-sheetTitle <- zen2han(dataSet[8,3])
-colnames(dataSet) <-
-  gsub('\\s|na','',sapply(paste0(dataSet[11,],dataSet[12,],dataSet[13,]),zen2han),ignore.case = T)
-
-dataSet[,1] <-
-  gsub('na','',sapply(paste0(dataSet[,1],dataSet[,2]),zen2han),ignore.case = T)
-dataSet <-
-  dataSet[dataSet[,1]!='',]
-dataSet <-
-  dataSet[,c(1,which(colnames(dataSet)!=''))]
-dataSet <-
-  dataSet[!is.na(dataSet[,2]),]
+assign('DirectInvestmentIncomeCredit',
+       merge(DirectInvestment1,DirectInvestment2, by = c('ID','地域'), all = T),
+       envir = .GlobalEnv)
+assign('DirectInvestmentIncomeDebit',
+       merge(DirectInvestment3,DirectInvestment4, by = c('ID','地域'), all = T),
+       envir = .GlobalEnv)
