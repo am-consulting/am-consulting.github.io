538dd53f12e3600592cbd3d6275d05bd750a6d20
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-01-15 02:12:59 +0900

    直接catで挟むと表示されないため修正
    直接catで挟むと表示されないため修正

--- R-markdownTable.r
+++ R-markdownTable.r
@@ -1,10 +1,12 @@
 fun_markdownTable <- function(obj, rownames = F, table.attr = "id = 'amcc' width = '100%'"){
-  cat('\n<div class = \'amccTableAutoScroll\'>\n')
-  knitr::kable(obj,
-               row.names = rownames,
-               align = 'r',
-               format = 'markdown',
-               format.args = list(big.mark = ',', drop0trailing = T),
-               table.attr = table.attr)
+  kableText <-
+    knitr::kable(obj,
+                 row.names = rownames,
+                 align = 'r',
+                 format = 'markdown',
+                 format.args = list(big.mark = ',', drop0trailing = T),
+                 table.attr = table.attr)
+  cat('\n<div class = "amccTableAutoScroll">\n')
+  print(kableText)
   cat('\n</div>\n')
 }
