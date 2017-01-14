e12a49c062ecec7c2af8ec6af54009ab7d96f44f
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-01-15 01:52:04 +0900

    tableをdivで囲む
    tableをdivで囲む

--- R-markdownTable.r
+++ R-markdownTable.r
@@ -1,8 +1,10 @@
 fun_markdownTable <- function(obj, rownames = F, table.attr = "id = 'amcc' width = '100%'"){
+  cat('\n<div class = \'amccTableAutoScroll\'>\n')
   knitr::kable(obj,
                row.names = rownames,
                align = 'r',
                format = 'markdown',
                format.args = list(big.mark = ',', drop0trailing = T),
                table.attr = table.attr)
+  cat('\n</div>\n')
 }
