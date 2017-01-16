1d41eaad108c8862ddf1632b257289fd8e106670
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-01-16 09:22:18 +0900

    2016年版倒産解散件数取得コード新規アップロード
    2016年版倒産解散件数取得コード新規アップロード

---
+++ importBankruptcyAndDissolution-2016.r
@@ -0,0 +1,37 @@
+library(XLConnect)
+username <-
+  Sys.info()['user']
+pathOutput <-
+  paste0("C:/Users/", username, "/Desktop/R_Data_Write/")
+setwd(pathOutput)
+sheetTitle <- vector()
+baseURL <-
+  'http://www.chusho.meti.go.jp/pamflet/hakusyo/H28/h28/excel/'
+fileName <-
+  c('b1_2_06_01.xlsx','b1_2_06_02.xlsx')
+for(iii in 1:length(fileName)){
+  download.file(paste0(baseURL, fileName[iii]),fileName[iii], mode = 'wb')
+buf0 <-
+  readWorksheetFromFile(paste0(pathOutput, fileName[iii]), sheet = 1, check.names = F, header = F)
+  sheetTitle[iii] <- buf0[1,1]
+  colnames(buf0) <- buf0[3,]
+  buf0 <- buf0[-c(1:3),]
+  buf0 <- apply(buf0,2,function(x)as.numeric(gsub(',','',x)))
+  if(iii == 1){
+    buf0[,1] <-
+      rev(seq(tail(buf0[,1],1) + 2000,by = -1,length.out = nrow(buf0)))
+    assign('bankruptcyCase', buf0, envir = .GlobalEnv)
+  }
+  if(iii == 2){
+    buf0[,1] <- buf0[,1] + 2000
+    assign('dissolutionCase', buf0, envir = .GlobalEnv)
+  }
+}
+# http://www.chusho.meti.go.jp/pamflet/hakusyo/
+# http://www.chusho.meti.go.jp/pamflet/hakusyo/H28/h28/index.html
+# http://www.chusho.meti.go.jp/pamflet/hakusyo/H28/h28/html/b1_2_1_2.html
+# http://www.chusho.meti.go.jp/pamflet/hakusyo/H28/h28/excel/b1_2_06_01.xlsx
+# http://www.chusho.meti.go.jp/pamflet/hakusyo/H28/h28/excel/b1_2_06_02.xlsx
+# http://www.chusho.meti.go.jp/koukai/chousa/tousan/index.htm
+# http://www.chusho.meti.go.jp/koukai/chousa/index.html
+# 2016年版中小企業白書・小規模企業白書をまとめました(平成28年4月22日)
