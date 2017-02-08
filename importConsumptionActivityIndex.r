1c583c243829eaa52e4b10b8a71de92ac3777f88
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-02-08 12:44:58 +0900

    消費活動指数取得コード新規アップロード
    消費活動指数取得コード新規アップロード

---
+++ importConsumptionActivityIndex.r
@@ -0,0 +1,32 @@
+# http://www.boj.or.jp/research/research_data/cai/index.htm/
+library(Nippon)
+username <-
+  Sys.info()['user']
+pathOutput <-
+  paste0("C:/Users/", username, "/Desktop/R_Data_Write/")
+setwd(pathOutput)
+fileName <-
+  'cai.xlsx'
+baseURL <-
+  'http://www.boj.or.jp/research/research_data/cai/'
+if(!file.exists(paste0(pathOutput, fileName))) {
+  download.file(url = paste0(baseURL,fileName),
+                destfile = fileName,
+                mode = 'wb')
+}
+sheetNo <- 1
+buf0 <-
+  XLConnect::readWorksheetFromFile(file = paste0(pathOutput, fileName),
+                                   sheet = sheetNo,
+                                   check.names = F,
+                                   header = F)
+colnames(buf0) <-
+  paste0(buf0[2,],':',buf0[3,])
+buf1 <-
+  buf0[!is.na(as.numeric(substring(buf0[,1],1,4))),]
+buf1[,1] <-
+  as.Date(paste0(substring(buf1[,1],1,4),'-',substring(buf1[,1],6,7),'-',substring(buf1[,1],9,10)))
+colnames(buf1)[1] <- 'Date'
+buf1[,-1] <-
+  apply(buf1[,-1],2,as.numeric)
+assign('ConsumptionActivityIndex',buf1,envir = .GlobalEnv)
