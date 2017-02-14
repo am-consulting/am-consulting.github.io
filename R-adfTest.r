b0034b594edab1079dce3cb9f0cd40077462f25d
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-02-14 20:41:50 +0900

    単位根検定関数新規アップロード
    単位根検定関数新規アップロード

---
+++ R-adfTest.r
@@ -0,0 +1,11 @@
+fun_adfTest <-
+  function(obj, objColumn = 2, lag = 1, diff = 1){
+    library(tseries)
+    cat('原数値\n')
+    print(apply(obj[,objColumn,drop = F],2,function(x)adf.test(x = x)))
+    cat('一次差分\n')
+    fun_makeDiffTable(obj = obj,
+                      lag = lag,
+                      diff = diff)
+    print(apply(diffDF[,objColumn,drop = F],2,function(x)adf.test(x = x)))
+  }
