8ed324860009cb48d3c623f66beff9da182ecb0e
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-02-24 10:10:11 +0900

    結果テキスト追記
    結果テキスト追記

--- R-turningPoint.r
+++ R-turningPoint.r
@@ -1,4 +1,4 @@
-fun_turningPoint <- function(obj, dateFormat = '%Y-%m'){
+fun_turningPoint <- function(obj, dateFormat = '%Y-%m', unit = '月', judge = c('輸入超','輸出超')){
   obj[,1] <- as.Date(obj[,1])
   obj0 <- obj
   obj0[obj0 == 0] <- 1
@@ -12,4 +12,26 @@ fun_turningPoint <- function(obj, dateFormat = '%Y-%m'){
   assign('durationExclude0', nrow(subset(obj, tail(obj0[,1],1) <= obj[,1])), envir = .GlobalEnv)
   obj0[,1] <-  format(obj0[,1],dateFormat)
   assign('objPNexclude0', obj0, envir = .GlobalEnv)
+  assign('texInclude0',paste0(tail(objPNinclude0[,1],1),
+                              'から',
+                              format(tail(obj[,1],1), dateFormat),
+                              'まで',
+                              durationInclude0,
+                              unit,
+                              '連続で',
+                              ifelse(tail(objPNinclude0[,2],1) < 0,
+                                     judge[1],
+                                     judge[2])),
+         envir = .GlobalEnv)
+  assign('texExclude0',paste0(tail(objPNexclude0[,1],1),
+                              'から',
+                              format(tail(obj[,1],1), dateFormat),
+                              'まで',
+                              durationExclude0,
+                              unit,
+                              '連続で',
+                              ifelse(tail(objPNexclude0[,2],1) < 0,
+                                     judge[1],
+                                     judge[2])),
+         envir = .GlobalEnv)
 }
