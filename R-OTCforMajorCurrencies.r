546ffb9e5af0e627d09031fa144a3a2949702850
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-02-24 11:39:11 +0900

    店頭外国為替証拠金取引月次出来高主要通貨建玉取得コード新規アップロード
    店頭外国為替証拠金取引月次出来高主要通貨建玉取得コード新規アップロード

---
+++ R-OTCforMajorCurrencies.r
@@ -0,0 +1,44 @@
+fun_MonthlyTradingVolumeOTCforMajorCurrencies <- function(sheetNo = 1){
+library(Nippon)
+username <-
+  Sys.info()['user']
+pathOutput <-
+  paste0("C:/Users/", username, "/Desktop/R_Data_Write/")
+setwd(pathOutput)
+fileName <-
+  'open_position_with_mc.xls'
+baseURL <-
+  'http://www.ffaj.or.jp/performance/fx_flash_file/data/'
+if(!file.exists(paste0(pathOutput, fileName))) {
+  download.file(url = paste0(baseURL,fileName),
+                destfile = fileName,
+                mode = 'wb')
+}
+
+
+buf0 <-
+  XLConnect::readWorksheetFromFile(file = paste0(pathOutput, fileName),
+                                   sheet = sheetNo,
+                                   check.names = F,
+                                   header = F)
+sheetTitle <-
+  gsub('\\s','',zen2han(paste0(buf0[1,1],':',buf0[4,1],':',buf0[7,1])))
+tmp <- NA
+for(ccc in 1:ncol(buf0)){
+  if(!is.na(buf0[9,ccc])){tmp <- buf0[9,ccc]}
+  buf0[9,ccc] <- tmp
+}
+colnames(buf0) <-
+  gsub('::',':',gsub('\n|[a-z]|\\s','',paste0(buf0[9,],':',buf0[10,]),ignore.case = T))
+buf1 <-
+  buf0[!is.na(as.numeric(buf0[,2])),]
+buf1[,1] <-
+  as.Date(paste0(buf1[,2],'-',buf1[,1],'-1'))
+buf2 <-
+  buf1[,-2]
+colnames(buf2)[1] <- 'Date'
+buf2[,-1] <- apply(buf2[,-1],2,function(x)as.numeric(gsub(',','',x)))
+buf3 <-
+  buf2[order(buf2[,1],decreasing = F),]
+assign('OTCforMajorCurrencies',buf3,envir = .GlobalEnv)
+}
