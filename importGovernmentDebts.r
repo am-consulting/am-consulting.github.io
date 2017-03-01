ae7d6582da0bf04c4496a6edd5ac698248742005
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-03-01 21:28:17 +0900

    政府債務取得コード新規アップロード
    政府債務取得コード新規アップロード

---
+++ importGovernmentDebts.r
@@ -0,0 +1,61 @@
+library(Nippon)
+username <-
+  Sys.info()['user']
+pathOutput <-
+  paste0("C:/Users/", username, "/Desktop/R_Data_Write/")
+setwd(pathOutput)
+fileName <-
+  'suii.xls'
+baseURL <-
+  'http://www.mof.go.jp/jgbs/reference/gbb/'
+if(!file.exists(paste0(pathOutput, fileName))) {
+  download.file(url = paste0(baseURL,fileName),
+                destfile = fileName,
+                mode = 'wb')
+}
+buf0 <-
+  XLConnect::readWorksheetFromFile(file = paste0(pathOutput, fileName),
+                                   sheet = 1,
+                                   check.names = F,
+                                   header = F)
+buf1 <- t(buf0)
+sheetTitle <-
+  zen2han(buf1[1,1])
+sheetUnit <-
+  zen2han(buf1[23,2])
+tmp <- NA
+for(ccc in 1:ncol(buf1)){
+  if(!is.na(buf1[1,ccc])){tmp <- buf1[1,ccc]}
+  buf1[1,ccc] <- tmp
+}
+tmp <- NA
+for(ccc in 1:ncol(buf1)){
+  if(!is.na(buf1[2,ccc])){tmp <- buf1[2,ccc]}
+  buf1[2,ccc] <- tmp
+}
+colnames(buf1) <-
+  sapply(gsub('\\s','',paste0(buf1[1,],':',buf1[2,],':',buf1[3,])),zen2han)
+buf2 <- buf1[-c(1:3),-c(1:2)]
+objColumn <-
+  grep('末',buf2[1,])
+buf2 <-
+  buf2[,-objColumn[2]]
+buf2[,-1] <-
+  apply(buf2[,-1],2,function(x)as.numeric(gsub(',|\\(|\\)|－','',x)))
+buf3 <-
+  buf2[,apply(buf2,2,function(x)sum(is.na(x)) != nrow(buf2))]
+yyyy <-
+  as.numeric(substring(text = buf3[,1],first = 2,last = 3)) + 1988
+mm <-
+  as.numeric(gsub('.+\\.([0-9]+)末','\\1',buf3[,1]))
+buf4 <-
+  data.frame(Date = as.Date(paste0(yyyy,'-',mm,'-1')),
+             buf3[,-1],
+             stringsAsFactors = F,
+             check.names = F,
+             row.names = NULL)
+colnames(buf4) <-
+  gsub(':na','',colnames(buf4),ignore.case = T)
+buf4[,-1] <-
+  apply(buf4[,-1],2,as.numeric)
+assign('GovernmentDebts',buf4,envir = .GlobalEnv)
