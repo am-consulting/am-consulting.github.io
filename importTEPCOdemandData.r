f3174b3bf8e670175c3b58a082395948f18081ff
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-03-05 23:13:09 +0900

    東京電力電力使用実績データ取得コード新規アップロード
    東京電力電力使用実績データ取得コード新規アップロード

---
+++ importTEPCOdemandData.r
@@ -0,0 +1,64 @@
+fun_TEPCOdemand <- function(plot = 0){
+  # http://www.tepco.co.jp/forecast/html/area_data-j.html
+  buf0 <-
+    read.csv(file = 'http://www.tepco.co.jp/forecast/html/images/area-2016.csv',
+             as.is = T,
+             stringsAsFactors = F,
+             header = F)
+  buf0[buf0 == ""] <- NA
+  tmp <- NA
+  for(ccc in 1:ncol(buf0)){
+    if(!is.na(buf0[2,ccc])){tmp <- buf0[2,ccc]}
+    buf0[2,ccc] <- tmp
+  }
+  colnames(buf0) <-
+    paste0(buf0[2,],':',buf0[3,])
+  colnames(buf0) <-
+    gsub(':na','',colnames(buf0),ignore.case = T)
+  buf1 <- buf0[-c(1:3),]
+  buf1[,1] <-
+    as.POSIXct(paste0(buf1[,1],' ',buf1[,2]))
+  buf2 <- buf1[,-2]
+  buf2[,-1] <-
+    apply(buf2[,-1],2,function(x)as.numeric(gsub(',','',x)))
+  assign('areaDemand',buf2,envir = .GlobalEnv)
+  # http://www.tepco.co.jp/forecast/html/download-j.html
+  buf2016 <-
+    read.csv(file = 'http://www.tepco.co.jp/forecast/html/images/juyo-2016.csv',
+             as.is = T,
+             stringsAsFactors = F,
+             header = T,
+             skip = 1)
+  buf2017 <-
+    read.csv(file = 'http://www.tepco.co.jp/forecast/html/images/juyo-2017.csv',
+             as.is = T,
+             stringsAsFactors = F,
+             header = T,
+             skip = 1)
+  buf0 <- rbind(buf2016,buf2017)
+  buf0[,1] <-
+    as.POSIXct(paste0(buf0[,1],' ',buf0[,2]))
+  buf1 <- buf0[,-2]
+  assign('actualElectricityDemand',buf1,envir = .GlobalEnv)
+  if(plot != 0){
+    par(mfrow = c(1,2))
+    plot(x = areaDemand[,1],
+         y = areaDemand[,2],
+         type = 'l',
+         xaxt = 'n',
+         xlab = '',
+         ylab = colnames(areaDemand)[2])
+    axis.POSIXct(side = 1,
+                 x = areaDemand[,1],
+                 format = '%Y-%m')
+    plot(x = actualElectricityDemand[,1],
+         y = actualElectricityDemand[,2],
+         type = 'l',
+         xaxt = 'n',
+         xlab = '',
+         ylab = colnames(actualElectricityDemand)[2])
+    axis.POSIXct(side = 1,
+                 x = actualElectricityDemand[,1],
+                 format = '%Y-%m')
+  }
+}
