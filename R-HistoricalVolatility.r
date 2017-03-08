a19718c1e11fe73e476be0c9cc39607cd2e14488
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-03-08 16:58:55 +0900

    ヒストリカルボラティリティ関数新規アップロード
    ヒストリカルボラティリティ関数新規アップロード

---
+++ R-HistoricalVolatility.r
@@ -0,0 +1,16 @@
+fun_historicalVolatility <-
+  function(obj, objColumn = 2, width = 30){
+    HV <- vector()
+    for(rrr in width:nrow(obj)){
+      tmp <-
+        obj[c((rrr-width+1):rrr),c(1,objColumn)]
+      fun_makeDiffRatioTable(obj = tmp,lag = 1,roundDigits = 3)
+      HV[rrr] <- sd(diffRatioDF[,2]/100) * sqrt(260) * 100
+    }
+    assign('historicalVolatility',
+           data.frame(Date = tail(obj[,1],-(width-1)),
+                      `HistoricalVolatility(%)` = na.omit(HV),
+                      stringsAsFactors = F,
+                      check.names = F),
+           envir = .GlobalEnv)
+  }
