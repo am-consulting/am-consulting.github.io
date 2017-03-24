a728d44beaff71384c637c21222b6a9d983ef704
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-03-24 17:28:16 +0900

    LIBORおよびTEDスプレッド取得コード新規アップロード
    LIBORおよびTEDスプレッド取得コード新規アップロード

---
+++ importLIBORandTED.r
@@ -0,0 +1,27 @@
+library(quantmod)
+fredData <- list()
+cnt <- 0
+treasuryRate <-
+  c(
+    'USD1MTD156N', '1-Month London Interbank Offered Rate (LIBOR), based on U.S. Dollar',
+    'USD3MTD156N', '3-Month London Interbank Offered Rate (LIBOR), based on U.S. Dollar',
+    'USD6MTD156N', '6-Month London Interbank Offered Rate (LIBOR), based on U.S. Dollar',
+    'USD12MD156N', '12-Month London Interbank Offered Rate (LIBOR), based on U.S. Dollar',
+    'TEDRATE', 'TED Spread'
+  )
+for (lll in seq(1, length(treasuryRate), by = 2)) {
+  cnt <- cnt + 1
+  tmp <- getSymbols(treasuryRate[lll], src = 'FRED', auto.assign = F)
+  tmp <-
+    data.frame(
+      Date = as.Date(index(tmp)),
+      tmp[, 1],
+      check.names = F,
+      row.names = NULL,
+      stringsAsFactors = F
+    )
+  colnames(tmp)[2] <- treasuryRate[lll + 1]
+  fredData[[cnt]] <- tmp
+  tail(fredData[[cnt]])
+  if(lll == 1){allData <- tmp}else{allData <- merge(allData, tmp, by='Date', all=T)}
+}
