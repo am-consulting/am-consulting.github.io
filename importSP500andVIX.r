310c8efffbf33a9eac181597925449d21db78eea
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-04-06 14:27:59 +0900

    S&P500、VIX取得コード新規アップロード
    S&P500、VIX取得コード新規アップロード

---
+++ importSP500andVIX.r
@@ -0,0 +1,10 @@
+library(quantmod)
+tmp <-
+  getSymbols(Symbols = '^GSPC',auto.assign = F)
+SP500 <-
+  data.frame(Date = index(tmp),tmp[,-1],stringsAsFactors = F,row.names = NULL)
+colnames(SP500) <- gsub('gspc','S&P500',colnames(SP500),ignore.case = T)
+tmp <-
+  getSymbols(Symbols = '^VIX',auto.assign = F)
+VIX <-
+  data.frame(Date = index(tmp),tmp[,-1],stringsAsFactors = F,row.names = NULL)
