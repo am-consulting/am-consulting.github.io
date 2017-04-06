eddabc3888c616c7bc6e293597552276a367e84e
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-04-06 15:02:42 +0900

    xtsデータ追記
    xtsデータ追記

--- importSP500andVIX.r
+++ importSP500andVIX.r
@@ -1,10 +1,14 @@
 library(quantmod)
 tmp <-
   getSymbols(Symbols = '^GSPC',auto.assign = F)
+colnames(tmp) <-
+  gsub('gspc','S&P500',colnames(tmp),ignore.case = T)
+SP500xts <- tmp
 SP500 <-
-  data.frame(Date = index(tmp),tmp,stringsAsFactors = F,row.names = NULL)
-colnames(SP500) <- gsub('gspc','S&P500',colnames(SP500),ignore.case = T)
+  data.frame(Date = index(tmp),tmp,stringsAsFactors = F,row.names = NULL,check.names = F)
+
 tmp <-
   getSymbols(Symbols = '^VIX',auto.assign = F)
+VIXxts <- tmp
 VIX <-
-  data.frame(Date = index(tmp),tmp,stringsAsFactors = F,row.names = NULL)
+  data.frame(Date = index(tmp),tmp,stringsAsFactors = F,row.names = NULL,check.names = F)
