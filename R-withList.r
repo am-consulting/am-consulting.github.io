58f125038da9d21c6d7542998e76f67f31ea7816
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-04-10 12:13:25 +0900

    総理大臣他追記コード新規アップロード
    総理大臣他追記コード新規アップロード

---
+++ R-withList.r
@@ -0,0 +1,18 @@
+fun_withList <- function(obj){
+csvFile <-
+  c('primeMinisterOfJapan.csv','boj.csv','potus.csv','frb.csv')
+baseURL <-
+  'https://raw.githubusercontent.com/am-consulting/am-consulting.github.io/master/csv/ListOfPresidents/'
+for(iii in seq(length(csvFile))){
+  listDF <-
+    read.csv(file = paste0(baseURL,csvFile[iii]),header = T,
+             quote = "\"",stringsAsFactors = F,check.names = F,fileEncoding = 'utf8')
+  objRow <-
+    sapply(obj[,1],function(x)which(listDF[,1] <= x & x < listDF[,2]))
+  if(iii==1){buf <- listDF[objRow,3]}else{buf <- cbind(buf,listDF[objRow,3])}
+}
+colnames(buf) <- toupper(gsub('.csv','',csvFile))
+bufDF <-
+  data.frame(obj,buf,stringsAsFactors = F,check.names = F,row.names = NULL)
+assign('dfWithList',bufDF,envir = .GlobalEnv)
+}
