b6488cd886e136fe16b4a8624c13be89f576142b
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-04-10 13:25:53 +0900

    引数escape追記
    引数escape追記

--- R-markdownTable.r
+++ R-markdownTable.r
@@ -1,11 +1,13 @@
-fun_markdownTable <- function(obj, rownames = F, table.attr = "id = 'amcc' width = '100%'", format = 'markdown'){
+fun_markdownTable <-
+  function(obj, rownames = F, table.attr = "id = 'amcc' width = '100%'", format = 'markdown', escape = F){
   kableText <-
     knitr::kable(obj,
                  row.names = rownames,
                  align = 'r',
                  format = format,
                  format.args = list(big.mark = ',', drop0trailing = T),
-                 table.attr = table.attr)
+                 table.attr = table.attr,
+                 escape = escape)
   cat('\n<div class = "amccTableAutoScroll">\n')
   print(kableText)
   cat('\n</div>\n')
