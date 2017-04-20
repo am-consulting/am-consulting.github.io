daa015f4ce938ce6acc467905c79c89abc643875
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-04-20 10:39:12 +0900

    季節調整済み輸出入額取得コード新規アップロード
    季節調整済み輸出入額取得コード新規アップロード

---
+++ importExportAndImportSA.r
@@ -0,0 +1,29 @@
+# Source http://www.customs.go.jp/toukei/shinbun/trade-st/sadj.htm
+library(rvest);library(lubridate)
+htmlMarkup <-
+  read_html(x = 'http://www.customs.go.jp/toukei/shinbun/trade-st/sadj.htm',
+            encoding = 'shift_jis')
+hrefLink <-
+  htmlMarkup %>% html_nodes('a') %>% html_attr('href')
+csvFile <-
+  hrefLink[grep('\\.csv',hrefLink)]
+buf0 <-
+  read.csv(file = paste0('http://www.customs.go.jp/toukei/shinbun/trade-st/',csvFile),
+           header = F,
+           quote = "\"",
+           check.names = F,stringsAsFactors = F,fileEncoding = 'shift_jis')
+notes01 <- buf0[1,1]
+colnames(buf0) <- buf0[3,]
+buf1 <- buf0[-c(1:4),]
+Date <-
+  as.Date(gsub('([0-9]+)/([0-9]+)','\\1-\\2-1',buf1[,1]))
+buf2 <-
+  data.frame(Date,apply(buf1[,-1],2,as.numeric),stringsAsFactors = F,row.names = NULL,check.names = F)
+if(length(grep('百万円',notes01))!=0){
+  buf2[,-1] <-
+    apply(buf2[,-1],2,function(x)x*10^-6)
+  colnames(buf2)[-1] <- paste0(colnames(buf2)[-1],'(Trillion JPY)')
+}else{
+  buf2 <- 0
+}
+assign('ExportAndImportSA',buf2,envir = .GlobalEnv)
