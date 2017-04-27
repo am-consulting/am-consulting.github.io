a0e19c4c7b6804dfa72ac45ee3ad3f35cf78e2cd
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-04-27 22:15:43 +0900

    データ最終日が現職の最終日と重なる場合の処理、unlist処理
    データ最終日が現職の最終日と重なる場合の処理、unlist処理

--- R-withList.r
+++ R-withList.r
@@ -8,7 +8,8 @@ for(iii in seq(length(csvFile))){
     read.csv(file = paste0(baseURL,csvFile[iii]),header = T,
              quote = "\"",stringsAsFactors = F,check.names = F,fileEncoding = 'utf8')
   objRow <-
-    sapply(obj[,1],function(x)which(listDF[,1] <= x & x < listDF[,2]))
+    sapply(obj[,1],function(x)tail(which(as.Date(listDF[,1]) <= x & x <= as.Date(listDF[,2])),1))
+  objRow <- unlist(objRow)
   if(iii==1){buf <- listDF[objRow,3]}else{buf <- cbind(buf,listDF[objRow,3])}
 }
 colnames(buf) <- toupper(gsub('.csv','',csvFile))
