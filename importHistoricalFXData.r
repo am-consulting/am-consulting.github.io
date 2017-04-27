6d2b58918757c29c0cd12877eefa209610293f29
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-04-27 15:20:32 +0900

    ヒストリカルFXデータ取得コード新規アップロード
    ヒストリカルFXデータ取得コード新規アップロード

---
+++ importHistoricalFXData.r
@@ -0,0 +1,14 @@
+library(Nippon)
+historicalFXDataURL <-
+  'https://www.mizuhobank.co.jp/rate/market/csv/quote.csv'
+historicalFXData0 <-
+  read.csv(file = historicalFXDataURL,header = F,
+           quote = "\"",na.strings = c('','*****'),check.names = F,stringsAsFactors = F,skip = 1)
+colnames(historicalFXData0) <-
+  sapply(paste0(historicalFXData0[1,],':',historicalFXData0[2,]),zen2han)
+historicalFXData0 <- historicalFXData0[-c(1:2),]
+historicalFXData <-
+  data.frame(Date=as.Date(historicalFXData0[,1]),
+             apply(historicalFXData0[,-1],2,as.numeric),
+             stringsAsFactors = F,check.names = F,row.names = NULL)
+colnames(historicalFXData) <- colnames(historicalFXData0)
