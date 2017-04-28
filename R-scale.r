d9244f2208490ab1b99861ad6f7303098448f8b7
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-04-28 20:49:15 +0900

    グローバル変数対策
    グローバル変数対策

--- R-scale.r
+++ R-scale.r
@@ -1,5 +1,7 @@
 fun_scale <-
   function(obj,center = T,scale = T){
+    if(exists('resultScaleDF',envir = .GlobalEnv)){remove('resultScaleDF',envir = .GlobalEnv)}
+    if(exists('resultScale',envir = .GlobalEnv)){remove('resultScale',envir = .GlobalEnv)}
     resultScale0 <-
       scale(obj[,-1], center = center, scale = scale)
     resultScaleDF <-
@@ -13,6 +15,8 @@ fun_scale <-
                  stringsAsFactors = F,
                  check.names = F,
                  row.names = NULL)
-    assign('resultScaleDF', resultScaleDF, envir = .GlobalEnv)
-    assign('resultScale',   resultScale,   envir = .GlobalEnv)
+    # assign('resultScaleDF', resultScaleDF, envir = .GlobalEnv)
+    # assign('resultScale',   resultScale,   envir = .GlobalEnv)
+    resultList <- list("resultScaleDF" = resultScaleDF, "resultScale" = resultScale)
+    return(resultList)
   }
