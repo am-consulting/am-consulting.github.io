e25d0578e2f63001ca1e12b9524ec32662fb40f2
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-04-28 12:40:43 +0900

    tDFが存在する場合はremoveに変更
    tDFが存在する場合はremoveに変更

--- transposeDataFrame.r
+++ transposeDataFrame.r
@@ -1,4 +1,5 @@
 fun_transposeDataFrame <- function(obj, tailN = 5, dateFormat = '%Y-%m-%d', colName = 'Item'){
+  if(exists('tDF')){remove('tDF',envir = .GlobalEnv)}
   if(dateFormat!=0){obj[,1] <- format(obj[,1], dateFormat)}
   buf <- t(tail(obj, tailN))
   buf <- data.frame(row.names(buf), buf, check.names = F, stringsAsFactors = F, row.names = NULL)
@@ -6,6 +7,7 @@ fun_transposeDataFrame <- function(obj, tailN = 5, dateFormat = '%Y-%m-%d', colN
   buf <- buf[-1,]
   colnames(buf)[1] <- colName
   buf[,-1] <- apply(buf[,-1,drop=F], 2, as.numeric)
-  # assign('tDF',buf,envir = .GlobalEnv)
-  return(buf)
+  assign('tDF',buf,envir = .GlobalEnv)
+  # return(buf)
 }
+# http://stackoverflow.com/questions/4837477/remove-objects-in-globalenv-from-within-a-function
