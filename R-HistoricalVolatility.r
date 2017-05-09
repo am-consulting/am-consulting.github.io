7be949f0299424401d9dfd6fb5ce5458a8a1693f
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-05-09 21:43:04 +0900

    グローバル変数対策
    グローバル変数対策

--- R-HistoricalVolatility.r
+++ R-HistoricalVolatility.r
@@ -4,13 +4,20 @@ fun_historicalVolatility <-
     for(rrr in width:nrow(obj)){
       tmp <-
         obj[c((rrr-width+1):rrr),c(1,objColumn)]
-      fun_makeDiffRatioTable(obj = tmp,lag = 1,roundDigits = 3)
+      diffRatioDF <-
+        fun_makeDiffRatioTable(obj = tmp,lag = 1,roundDigits = 3)
       HV[rrr] <- sd(diffRatioDF[,2]/100) * sqrt(260) * 100
     }
-    assign('historicalVolatility',
-           data.frame(Date = tail(obj[,1],-(width-1)),
-                      `HistoricalVolatility(%)` = na.omit(HV),
-                      stringsAsFactors = F,
-                      check.names = F),
-           envir = .GlobalEnv)
+    # assign('historicalVolatility',
+    #        data.frame(Date = tail(obj[,1],-(width-1)),
+    #                   `HistoricalVolatility(%)` = na.omit(HV),
+    #                   stringsAsFactors = F,
+    #                   check.names = F),
+    #        envir = .GlobalEnv)
+    historicalVolatility <-
+      data.frame(Date = tail(obj[,1],-(width-1)),
+                 `HistoricalVolatility(%)` = na.omit(HV),
+                 stringsAsFactors = F,
+                 check.names = F)
+    return(historicalVolatility)
   }
