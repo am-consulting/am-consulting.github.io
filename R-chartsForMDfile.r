3076dd45faf65f8d909eb3ef13af901799f6a46a
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-05-25 14:02:16 +0900

    mdファイル向けチャート生成コード新規アップロード
    mdファイル向けチャート生成コード新規アップロード

---
+++ R-chartsForMDfile.r
@@ -0,0 +1,45 @@
+fun_preparePlot <- function(){
+  username <-
+    Sys.info()['user']
+  pathToFile <-
+    paste0('C:/Users/', username,'/Desktop/pathToCSV/')
+  setwd(pathToFile)
+  fileName <-
+    'defaultPath.csv'
+  buf <-
+    read.csv(file = fileName,
+             header = F,
+             skip = 0,
+             stringsAsFactor = F,
+             check.names = F,
+             fileEncoding = 'utf-8',quote = "\"")
+  pathOutputTOchart <-
+    paste0("C:/Users/", username, buf[2,1],'charts/chartImages/')
+  setwd(pathOutputTOchart)
+  getwd()
+}
+
+fun_PlotStrawAnd4charts <-
+  function(obj,varType=1,width=1000,height=600,
+           dateFormat='%Y-%m',tailN=c(10^10,12*5,12*3,12),chartType='o'){
+    fun_preparePlot()
+    if(exists('htmlName')){
+      pngFile <- paste0(htmlName, "1.png")
+      png(file = pngFile, width = width, height = height)
+    }
+    par(mfrow = c(1,1),family = 'Meiryo')
+    tmp0 <- fun_tsDataByYear(objDF = obj,dateCol = 1,dataCol = 2)
+    tmp1 <- fun_strawBroomData(objDF = tmp0,varType = 1)
+    fun_plotStrawBroomByYear(strawData = tmp1$strawData,
+                             lastDate = tmp1$lastDate,
+                             variation = tmp1$variation)
+    if(exists('htmlName')){
+      pngFile <- paste0(htmlName, "2.png")
+      png(file = pngFile, width = width, height = height)
+    }
+    par(mfrow = c(1,1),family = 'Meiryo')
+    fun_plot4charts(obj = obj,
+                    dateFormat = dateFormat,
+                    tailN = tailN,
+                    type = chartType)
+}
