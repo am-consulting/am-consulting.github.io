4a84fc3a5c9cff26a20147e8ca8ac3775291da2e
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-05-26 11:48:35 +0900

    対象系列拡張
    対象系列拡張

--- importCreditToTheNonfinancialSector.r
+++ importCreditToTheNonfinancialSector.r
@@ -1,5 +1,6 @@
 # 世界各国の非金融部門(政府,企業,家計)債務残高対国内総生産(GDP)比
 # http://www.bis.org/statistics/totcredit.htm?m=6%7C326
+# Private non-financial sector = Households and NPISHs + Non-financial corporations
 username <-
   Sys.info()['user']
 pathOutput <-
@@ -13,11 +14,14 @@ buf0 <-
   gdata::read.xls(xls = paste0(pathOutput, fileName),
                   sheet = 3,stringsAsFactors = F,header = F,check.names = F,na.strings = '')
 buf1 <- buf0
-colnames(buf1) <- paste0(buf1[1,],':',buf1[2,])
-buf2 <- buf1[-c(1:grep('period',buf1[,1],ignore.case = T)),]
+buf1 <- buf1[,c(1,grep('per cent',buf1[2,],ignore.case = T))]
+buf1 <- buf1[,-grep('nominal',buf1[1,],ignore.case = T)]
 objCol <-
-  grep('Non financial sector - All sectors - Market value - Percentage of GDP - Adjusted for breaks',colnames(buf2))
-buf3 <- buf2[,c(1,objCol)]
+  grep('Non financial sector|General government|Households and NPISHs|Non-financial corporations|Private non-financial sector',
+       buf1[1,],ignore.case = T)
+buf2 <- buf1[,c(1,objCol)]
+colnames(buf2) <- paste0(buf2[1,],':',buf2[2,])
+buf3 <- buf2[-c(1:grep('period',buf2[,1],ignore.case = T)),]
 Date <- as.Date(gsub('([0-9]+)\\.([0-9]+)\\.([0-9]+)','\\3-\\2-\\1',buf3[,1]))
 CreditToTheNonfinancialSector <-
   data.frame(Date,apply(buf3[,-1],2,as.numeric),stringsAsFactors = F,check.names = F,row.names = NULL)
