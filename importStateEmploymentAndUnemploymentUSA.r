7c5878cdc8e0ba318e3540eb7f398d41559a6b1e
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-06-16 13:23:56 +0900

    StateEmploymentAndUnemployment取得コード新規アップロード
    StateEmploymentAndUnemployment取得コード新規アップロード

---
+++ importStateEmploymentAndUnemploymentUSA.r
@@ -0,0 +1,40 @@
+# https://www.bls.gov/news.release/empsit.toc.htm
+library(rvest)
+targetURL <-
+  'https://www.bls.gov/news.release/laus.htm'
+htmlMarkup <-
+  read_html(x = targetURL)
+objTableS <-
+  htmlMarkup %>% html_nodes('table')
+captionS <-
+  objTableS %>% html_nodes('caption') %>% html_text()
+captionS <-
+  gsub('\n|\\([0-9]+\\)','',captionS)
+captionS <-
+  gsub('([a-z])table','\\1 Table',captionS,ignore.case = T)
+captionS <-
+  gsub('DATANOT','DATA NOT',captionS,ignore.case = T)
+captionS <-
+  gsub('DATASEASONALLY','DATA SEASONALLY',captionS,ignore.case = T)
+tableList <-
+  objTableS %>% html_table(fill = T,header = F)
+StateEmploymentAndUnemployment <- list()
+for(iii in seq(length(tableList))){
+  buf0 <-
+    data.frame(tableList[[iii]],stringsAsFactors = F)
+  buf1 <- buf0
+  buf1[3,buf1[2,]==buf1[3,]] <- NA
+  buf1[2,buf1[1,]==buf1[2,]] <- NA
+  colnames(buf1) <- paste0(buf1[1,],':',buf1[2,],':',buf1[3,])
+  colnames(buf1) <- gsub(':na','',colnames(buf1),ignore.case = T)
+  buf2 <- buf1[-c(1:3),]
+  buf2[,-1] <-
+    apply(buf2[,-1],2,function(x)as.numeric(gsub(',','',x)))
+  buf3 <- buf2[!is.na(buf2[,2]),]
+  buf3[,1] <-
+    gsub('\\([0-9]+\\)','',buf3[,1])
+  print(buf3)
+  StateEmploymentAndUnemployment[[iii]] <- buf3
+  cat('\n\n#########################\n\n')
+}
+remove('buf0','buf1','buf2','buf3')
