ad1a7a76c846b1a95eb2e6f3e6dc000dd20ee196
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-06-25 23:49:37 +0900

    線形回帰を後ろ向きに変更、チャート仕樣部分変更
    線形回帰を後ろ向きに変更、チャート仕樣部分変更

--- R-ChangesInSlope.r
+++ R-ChangesInSlope.r
@@ -1,7 +1,7 @@
 fun_changesInSlope <- function(obj,objCol = 2,h = 12){
   slopeDF <- data.frame()
-  for(rrr in 1:(nrow(obj)-h+1)){
-    tmp <- obj[rrr:(rrr+h-1),c(1,objCol)]
+  for(rrr in h:nrow(obj)){
+    tmp <- obj[(rrr-h+1):rrr,c(1,objCol)]
     lmResult <- lm(tmp[,2]~tmp[,1])
     slopeDF[rrr,1] <- obj[rrr,1]
     slopeDF[rrr,2] <- lmResult$coefficients[2]
@@ -10,18 +10,21 @@ fun_changesInSlope <- function(obj,objCol = 2,h = 12){
     as.Date(slopeDF[,1])
   colnames(slopeDF) <-
     c('Date','Slope')
+  slopeDF <- na.omit(slopeDF)
   changesInConditions <-
     merge(obj[,c(1,objCol)],slopeDF,by = 'Date',all = 'T')
-  par(mar=c(4,5,3,5),family='Meiryo',font.main=1,cex.main=1)
+  par(mar=c(4,5,4,5),family='Meiryo',font.main=1,cex.main=1)
   plot(changesInConditions[,c(1,2)],type='l',col = '#4169e1',
-       xlab='',ylab=colnames(changesInConditions)[2],main=colnames(changesInConditions)[2],
+       xlab='',ylab=colnames(changesInConditions)[2],
+       main=paste0(colnames(changesInConditions)[2],'\n',
+                   paste0(format(range(changesInConditions[,1]),'%Y-%m'),collapse = '~'),'\nRange(months):',h),
        panel.first = grid(nx = NULL,ny = NULL,lty = 2,equilogs = T))
   par(new=T)
   plot(changesInConditions[,c(1,3)],type='h',col='#696969',
        xlab = '',ylab = '',xaxt = 'n',yaxt = 'n',
        panel.first = grid(nx = NULL,ny = NULL,lty = 2,equilogs = T))
   axis(side = 4,cex.axis = 1)
-  mtext(text = colnames(changesInConditions)[3],side = 4,line = 3.2,cex = 1)
+  mtext(text = colnames(changesInConditions)[3],side = 4,line = 2.2,cex = 1)
   changesInSlope <-
     na.omit(data.frame(head(changesInConditions,-1),
                        Product=head(changesInConditions$Slope,-1)*tail(changesInConditions$Slope,-1)))
