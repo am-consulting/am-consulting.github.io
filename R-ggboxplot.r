c9f50b349a7df75ddd8ccc99911cfd6f38b6574c
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-06-30 14:49:46 +0900

    引数datecol,datasource追加、各種theme追記
    引数datecol,datasource追加、各種theme追記

--- R-ggboxplot.r
+++ R-ggboxplot.r
@@ -3,8 +3,9 @@
 library(ggpubr)
 library(ggsci)
 fun_ggboxplot <-
-  function(obj,objCol = 2,ggtheme = theme_bw(),palette = "UChicago",add = "jitter",discreteN = 6){
-    plotData <- obj[,c(1,objCol)]
+  function(obj,dateCol = 1,objCol = 2,ggtheme = theme_bw(),palette = "UChicago",add = "jitter",discreteN = 6,
+           dataSource = dataSource){
+    plotData <- obj[,c(dateCol,objCol)]
     plotData$Year <-
       as.factor(lubridate::year(plotData[,1]))
     colnames(plotData)[2] <- 'Value'
@@ -13,7 +14,13 @@ fun_ggboxplot <-
     p <-
       ggboxplot(plotData,x = "Year",y = 'Value',color = "Year",palette = palette,
                 add = add,shape = "Year",ggtheme = ggtheme,repel = T)
-    p <- p + ggtitle(colnames(obj)[objCol])+ theme(plot.title = element_text(hjust = 0.5))
+    p <- p + xlab('') + ylab('') + theme(legend.position = "none")
+    p <- p + ggtitle(paste0(colnames(obj)[objCol],'\nSource:',dataSource))
+    p <- p + theme(plot.title =   element_text(hjust = 0.5,family = 'Meiryo',face = 'plain'))
+    p <- p + theme(legend.text =  element_text(hjust = 0.5,family = 'Meiryo',face = 'plain'))
+    p <- p + theme(axis.title.x = element_text(hjust = 0.5,family = 'Meiryo',face = 'plain'))
+    p <- p + theme(axis.title.y = element_text(hjust = 0.5,family = 'Meiryo',face = 'plain'))
+    p <- p + theme(axis.text =    element_text(hjust = 0.5,family = 'Meiryo',face = 'plain'))
     print(p)
     returnDF <-
       ts(data = plotData$Value,start = c(year(plotData[1,1]),month(plotData[1,1])),frequency = 12)
