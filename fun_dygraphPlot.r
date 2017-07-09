280d95781e3adcbe3af665cb39380464e98e719d
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-07-10 08:50:07 +0900

    引数fillgraph追記
    引数fillgraph追記

--- fun_dygraphPlot.r
+++ fun_dygraphPlot.r
@@ -1,5 +1,6 @@
 library(dygraphs)
-fun_dygraphPlot <- function(tsData,mainTitle = '',name = 'Dark2',group = 0,show = c("auto","always","onmouseover","follow","never"),width = 250,direction = c("both","horizontal","vertical"),maxNumberWidth = 100,axis = c("y","y2"),pointSize = 2,drawPoints = TRUE){
+library(xts)
+fun_dygraphPlot <- function(tsData,mainTitle = '',name = 'Dark2',group = 0,show = c("auto","always","onmouseover","follow","never"),width = 250,direction = c("both","horizontal","vertical"),maxNumberWidth = 100,axis = c("y","y2"),pointSize = 2,drawPoints = TRUE,fillGraph = c(TRUE,FALSE)){
   xtsData <- xts(tsData[,-1],order.by = tsData[,1])
   colnames(xtsData) <- colnames(tsData)[-1]
   colors <- RColorBrewer::brewer.pal(n = ncol(xtsData),name = name)
@@ -12,8 +13,11 @@ fun_dygraphPlot <- function(tsData,mainTitle = '',name = 'Dark2',group = 0,show 
   }
   for(ccc in seq(ncol(xtsData))){
     obj <- obj %>%
-      dySeries(colnames(xtsData)[ccc],drawPoints = drawPoints[ccc],pointSize = pointSize[ccc],axis = axis[ccc],color = colors[ccc])
-    # length(axis)よりncol(xtsData)が大きいとError in match.arg(axis, c("y", "y2")):'arg' should be one of “y”, “y2”がでる
+      dySeries(colnames(xtsData)[ccc],drawPoints = drawPoints[ccc],
+               pointSize = pointSize[ccc],axis = axis[ccc],color = colors[ccc],
+               fillGraph = fillGraph[ccc])
+    # length(axis)よりncol(xtsData)が大きいと、
+    # Error in match.arg(axis, c("y", "y2")):'arg' should be one of “y”, “y2”がでる
   }
   if(length(unique(axis))!=1){
     labelY <- paste0(colnames(xtsData)[grep('y$',axis)],collapse = ',')
@@ -23,7 +27,8 @@ fun_dygraphPlot <- function(tsData,mainTitle = '',name = 'Dark2',group = 0,show 
       dyAxis("y2",label = labelY2,independentTicks = T)
   }
   obj <- obj %>%
-    dyLegend(show = show,width = width,showZeroValues = 'TRUE',labelsSeparateLines = 'TRUE',hideOnMouseOut = 'FALSE') %>%
+    dyLegend(show = show,width = width,showZeroValues = 'TRUE',
+             labelsSeparateLines = 'TRUE',hideOnMouseOut = 'FALSE') %>%
     dyRangeSelector(keepMouseZoom = 'TRUE',retainDateWindow = 'FALSE') %>%
     dyUnzoom() %>%
     dyCrosshair(direction = direction) %>%
