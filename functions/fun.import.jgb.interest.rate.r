a1018cb1b73357dc97afcc5fce4a1df0d61ca85d
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-09-08 15:25:32 +0900

    Upload
    Code for import JGB interest rate data

---
+++ functions/fun.import.jgb.interest.rate.r
@@ -0,0 +1,19 @@
+fun.import.jgb.interest.rate <- function(){
+  base.url <- "http://www.mof.go.jp/english/jgbs/reference/interest_rate/"
+  csv.file <- c("historical/jgbcme_all.csv", "jgbcme.csv")
+  data.set <- list()
+  for(iii in seq(length(csv.file))){
+    Sys.sleep(1) # To prevent server overload
+    data.set[[iii]] <-
+      read.csv(paste0(base.url,csv.file[1]),header = T,skip = 1,stringsAsFactor = F,check.names = F,na.strings = '-')
+  }
+  if(colnames(data.set[[1]]) == colnames(data.set[[2]])){
+    tmp <- rbind(data.set[[1]],data.set[[2]])
+    tmp[,1] <- as.Date(tmp[,1])
+    jgb.interest.rate <-
+      data.frame(Date = tmp[,1],apply(tmp[,-1,drop = F],2,as.numeric),stringsAsFactors = F,check.names = F,row.names = NULL)
+    return(jgb.interest.rate)
+  }else{
+    stop('There is a difference in the column names of Current Data and Historical Data.')
+  }
+}
