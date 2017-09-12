5fb0526c1318db6c2bf39dcc36f6060a9167b49a
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-09-12 15:42:36 +0900

    Update
    変数修正

--- public.code/fun.convert.timeseries.r
+++ public.code/fun.convert.timeseries.r
@@ -1,19 +1,19 @@
 fun.convert.timeseries <- function(obj,index.name = 'Index'){
-  if(class(data.set)[1] == 'xts'){
+  if(class(obj)[1] == 'xts'){
     data.set.xts <- obj
     data.set.df <-
       data.frame(Date = index(data.set.xts),data.set.xts,check.names = F,row.names = NULL,stringsAsFactors = F)
     data.set.ts <-
       ts(data = data.set.xts,start = c(year(head(index(data.set.xts),1)),month(head(index(data.set.xts),1))),frequency = 12)
   }
-  if(class(data.set)[1] == 'data.frame'){
+  if(class(obj)[1] == 'data.frame'){
     data.set.df <- obj
     data.set.xts <- as.xts(data.set.df[,-1],order.by = as.Date(data.set.df[,1]))
     colnames(data.set.xts) <- colnames(data.set.df)[2]
     data.set.ts <-
       ts(data = data.set.xts,start = c(year(head(index(data.set.xts),1)),month(head(index(data.set.xts),1))),frequency = 12)
   }
-  if(class(data.set)[1] == 'ts'){
+  if(class(obj)[1] == 'ts'){
     data.set.ts <- obj
     data.set.df <- data.frame(as.Date(time(data.set.ts)),as.numeric(data.set.ts),stringsAsFactors = F,row.names = NULL)
     colnames(data.set.df) <- c('Date',index.name)
