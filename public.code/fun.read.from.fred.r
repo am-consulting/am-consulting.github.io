95c2d962ee2b456de1b7173f3f6d3577993da231
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-09-12 15:42:00 +0900

    Upload
    FREDからの時系列データ読み込み

---
+++ public.code/fun.read.from.fred.r
@@ -0,0 +1,27 @@
+fun.read.from.fred <- function(index = 'Dow Jones Industrial Average (DJIA)'){
+  index.title <-
+    gsub('(.+)\\((.+)\\)','\\1',index)
+  index.symbol <-
+    gsub('(.+)\\((.+)\\)','\\2',index)
+  data.set.xts <-
+    na.omit(getSymbols(Symbols = index.symbol,auto.assign = F,src = 'FRED'))
+  index.url <-
+    paste0('https://fred.stlouisfed.org/series/',index.symbol)
+  html.markup <-
+    read_html(x = index.url,encoding = 'utf8')
+  index.spec <-
+    html.markup %>% html_nodes(xpath = "//span[@class = 'series-meta-value']")
+  index.unit <-
+    index.spec[[grep('units',index.spec,ignore.case = T)[1]]] %>% html_text()
+  index.unit <-
+    gsub('\\s+$','',index.unit)
+  index.freq <-
+    index.spec[[grep('frequency',index.spec,ignore.case = T)[1]]] %>% html_text()
+  index.freq <-
+    gsub('\\s|\n','',index.freq)
+  returnList <-
+    list('data.set.xts' = data.set.xts,
+         'index.unit' = index.unit,
+         'index.freq' = index.freq)
+  return(returnList)
+}
