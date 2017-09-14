288ffb3efecd9b7e6aa348781a05ba38f3a2a284
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-09-14 20:52:38 +0900

    update
    日付を変更

--- public.code/fun.read.bis.statistics.r
+++ public.code/fun.read.bis.statistics.r
@@ -1,7 +1,7 @@
 fun.read.ilbs.icbs <- function(path.to.folder = '/Desktop/R_Data_Write/'){
   # The Results of BIS International Locational Banking Statistics and International Consolidated Banking Statistics in Japan
   # https://www.boj.or.jp/statistics/bis/ibs/index.htm/
-  # required package:XLConnect,Nippon
+  # required package:XLConnect,Nippon,lubridate
   buf.wd <- getwd()
   path.to.folder <- paste0('C:/Users/',Sys.info()['user'],path.to.folder)
   setwd(path.to.folder)
@@ -19,17 +19,18 @@ fun.read.ilbs.icbs <- function(path.to.folder = '/Desktop/R_Data_Write/'){
     buf1 <- buf0[,c(1,2,region.col.s[iii]:region.col.e[iii])]
     tmp <- NA;for(ccc in seq(ncol(buf1))){if(!is.na(buf1[3,ccc])){tmp <- buf1[3,ccc]};buf1[3,ccc] <- tmp}
     tmp <- NA;for(ccc in seq(ncol(buf1))){if(!is.na(buf1[4,ccc])){tmp <- buf1[4,ccc]};buf1[4,ccc] <- tmp}
-    region.col.ss <- which(!is.na(buf1[5,]))
-    region.col.ee <- c(region.col.ss[-1]-1,ncol(buf1))
-    for(iiii in seq(length(region.col.ss))){
-      buf2 <- buf1[,c(1,2,region.col.ss[iiii]:region.col.ee[iiii])]
+    currency.col.s <- which(!is.na(buf1[5,]))
+    currency.col.e <- c(currency.col.s[-1]-1,ncol(buf1))
+    for(iiii in seq(length(currency.col.s))){
+      buf2 <- buf1[,c(1,2,currency.col.s[iiii]:currency.col.e[iiii])]
       tmp <- NA;for(ccc in seq(ncol(buf2))){if(!is.na(buf2[5,ccc])){tmp <- buf2[5,ccc]};buf2[5,ccc] <- tmp}
       tmp <- NA;for(ccc in seq(ncol(buf2))){if(!is.na(buf2[6,ccc])){tmp <- buf2[6,ccc]};buf2[6,ccc] <- tmp}
       col.name <- sapply(paste0(buf2[3,],':',buf2[4,],':',buf2[5,],':',buf2[6,],':',buf2[7,]),zen2han)
       colnames(buf2) <- gsub('\\d+\\.|\\(\\d+\\)|:?NA','',col.name)
       buf3 <- buf2[which(!is.na(as.numeric(substring(buf2[,1],1,4))))[1]:nrow(buf2),]
       tmp <- NA;for(rrr in seq(nrow(buf3))){if(!is.na(buf3[rrr,1])){tmp <- buf3[rrr,1]};buf3[rrr,1] <- tmp}
-      buf3[,1] <- sapply(paste0(buf3[,1],buf3[,2]),zen2han)
+      buf3[,1] <-
+        as.Date(paste0(substring(buf3[,1],1,4),'-',gsub('月末','',buf3[,2]),'-1')) %m+% months(1) -1
       buf4 <- buf3[,-2]
       colnames(buf4)[1] <- 'Date'
       cnt <- cnt + 1
@@ -39,5 +40,9 @@ fun.read.ilbs.icbs <- function(path.to.folder = '/Desktop/R_Data_Write/'){
     }
   }
   setwd(buf.wd)
-  return(bis.data)
+  returnList <-
+    list('bis.data' = bis.data,
+         'sheet.title' = sheet.title,
+         'sheet.unit' = sheet.unit)
+  return(returnList)
 }
