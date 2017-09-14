e015cd25dd82519d0e72aca30eb43adb30bff793
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-09-14 16:27:28 +0900

    upload
    bis data

---
+++ public.code/fun.read.bis.statistics.r
@@ -0,0 +1,43 @@
+fun.read.ilbs.icbs <- function(path.to.folder = '/Desktop/R_Data_Write/'){
+  # The Results of BIS International Locational Banking Statistics and International Consolidated Banking Statistics in Japan
+  # https://www.boj.or.jp/statistics/bis/ibs/index.htm/
+  # required package:XLConnect,Nippon
+  buf.wd <- getwd()
+  path.to.folder <- paste0('C:/Users/',Sys.info()['user'],path.to.folder)
+  setwd(path.to.folder)
+  target.url <- 'https://www.boj.or.jp/statistics/bis/ibs/hbis11.xlsx'
+  xls.file <- gsub('.+/([^/]+)','\\1',target.url)
+  download.file(url = target.url,xls.file,mode = 'wb')
+  sheet.name <- getSheets(loadWorkbook(xls.file))
+  buf0 <- readWorksheetFromFile(xls.file,sheet = 2,check.names = F,header = F)
+  sheet.title <- zen2han(buf0[1,1]);sheet.unit <- zen2han(buf0[2,1])
+  region.col.s <- which(!is.na(buf0[3,]))
+  region.col.e <- c(region.col.s[-1]-1,ncol(buf0))
+  cnt <- 0
+  bis.data <- list()
+  for(iii in seq(length(region.col.s))){
+    buf1 <- buf0[,c(1,2,region.col.s[iii]:region.col.e[iii])]
+    tmp <- NA;for(ccc in seq(ncol(buf1))){if(!is.na(buf1[3,ccc])){tmp <- buf1[3,ccc]};buf1[3,ccc] <- tmp}
+    tmp <- NA;for(ccc in seq(ncol(buf1))){if(!is.na(buf1[4,ccc])){tmp <- buf1[4,ccc]};buf1[4,ccc] <- tmp}
+    region.col.ss <- which(!is.na(buf1[5,]))
+    region.col.ee <- c(region.col.ss[-1]-1,ncol(buf1))
+    for(iiii in seq(length(region.col.ss))){
+      buf2 <- buf1[,c(1,2,region.col.ss[iiii]:region.col.ee[iiii])]
+      tmp <- NA;for(ccc in seq(ncol(buf2))){if(!is.na(buf2[5,ccc])){tmp <- buf2[5,ccc]};buf2[5,ccc] <- tmp}
+      tmp <- NA;for(ccc in seq(ncol(buf2))){if(!is.na(buf2[6,ccc])){tmp <- buf2[6,ccc]};buf2[6,ccc] <- tmp}
+      col.name <- sapply(paste0(buf2[3,],':',buf2[4,],':',buf2[5,],':',buf2[6,],':',buf2[7,]),zen2han)
+      colnames(buf2) <- gsub('\\d+\\.|\\(\\d+\\)|:?NA','',col.name)
+      buf3 <- buf2[which(!is.na(as.numeric(substring(buf2[,1],1,4))))[1]:nrow(buf2),]
+      tmp <- NA;for(rrr in seq(nrow(buf3))){if(!is.na(buf3[rrr,1])){tmp <- buf3[rrr,1]};buf3[rrr,1] <- tmp}
+      buf3[,1] <- sapply(paste0(buf3[,1],buf3[,2]),zen2han)
+      buf4 <- buf3[,-2]
+      colnames(buf4)[1] <- 'Date'
+      cnt <- cnt + 1
+      bis.data[[cnt]] <-
+        data.frame(buf4[,1,drop=F],apply(buf4[,-1],2,function(x)as.numeric(gsub(',','',x))),
+                   stringsAsFactors = F,check.names = F,row.names = NULL)
+    }
+  }
+  setwd(buf.wd)
+  return(bis.data)
+}
