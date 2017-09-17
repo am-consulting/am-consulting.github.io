46547a87d9ad01eb698bf7938b1f0f6aee6954aa
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-09-17 17:05:22 +0900

    update
    historical volatility

--- public.code/fun.create.diff.ma.data.r
+++ public.code/fun.create.diff.ma.data.r
@@ -1,5 +1,5 @@
-fun.create.diff.ma.data <- function(obj,col.date = 1,col.target = 2,lag = 1,diff = 1,round.dig = 5,n = 12){
-  # required package:TTR
+fun.create.diff.ma.data <- function(load.library = F,obj,col.date = 1,col.target = 2,lag = 1,diff = 1,round.dig = 5,n = 12,historical.width = 30,time.ratio.daily = 260){
+  if(load.library==T){lapply(c('TTR','lubridate'),require,character.only = T)}
   obj <- obj[,c(col.date,col.target)]
   moving.ave <-
     data.frame(obj,SMA = SMA(obj[,2], n = n),EMA = EMA(obj[,2], n = n),
@@ -16,6 +16,20 @@ fun.create.diff.ma.data <- function(obj,col.date = 1,col.target = 2,lag = 1,diff
     gsub('diff.diff',paste0(colnames(obj)[2],'(差,',lag,',',diff,')'),colnames(diff.df))
   colnames(diff.df) <-
     gsub('diff.ratio',paste0(colnames(obj)[2],'(比,',lag,',',diff,')'),colnames(diff.df))
-  diff.ma.data <- merge(moving.ave,diff.df,all=T)
+  # historical volatility part
+  if(4 < length(unique(month(obj[,1])))){
+    if(1 < length(unique(day(obj[,1])))){time.ratio <- time.ratio.daily}else{time.ratio <- 12}
+  }
+  historical.volatility <- vector()
+  for(rrr in historical.width:nrow(diff.df)){
+    tmp <- diff.df[c((rrr-historical.width+1):rrr),grep('\\(比',colnames(diff.df))]
+    historical.volatility[rrr] <- sd(tmp/100)*sqrt(time.ratio)*100
+  }
+  historical.volatility.df <-
+    data.frame(diff.df[,1,drop=F],historical.volatility,
+               stringsAsFactors = F,check.names = F)
+  colnames(historical.volatility.df)[2] <- paste0('Historical Volatility(width:',historical.width,')')
+  # historical volatility part
+  diff.ma.data <- merge(merge(moving.ave,diff.df,all=T),historical.volatility.df,all=T)
   return(diff.ma.data)
 }
