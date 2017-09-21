de457bde48663a3bf0daaa1eafb579529b8fd79e
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-09-21 22:05:18 +0900

    update
    引数g,color #696969

--- public.code/fun.plot.ggplot.ann.line.text.r
+++ public.code/fun.plot.ggplot.ann.line.text.r
@@ -1,24 +1,24 @@
-fun.plot.ggplot.ann.rect <- function(xmin,xmax,ymin=-Inf,ymax=Inf,alpha=0.1,fill='red'){
+fun.plot.ggplot.ann.rect <- function(g=g,xmin,xmax,ymin=-Inf,ymax=Inf,alpha=0.1,fill='red'){
   g <- g + annotate('rect',xmin = xmin,xmax = xmax,ymin = ymin,ymax = ymax,alpha = alpha,fill = fill)
   return(g)
 }
 
-fun.plot.ggplot.ann.text <- function(x,y=Inf,lable,angle=0,vjust=1,hjust=0,size=7,col='black',family='Meiryo'){
+fun.plot.ggplot.ann.text <- function(g=g,x,y=Inf,label,angle=0,vjust=1,hjust=0,size=7,col='#696969',family='Meiryo'){
   g <- g + annotate('text',x = x,y = y,label = label,angle = angle,vjust = vjust,hjust = hjust,size = size,col = col,family = family)
   return(g)
 }
 
-fun.plot.ggplot.vline <- function(xintercept,col='#696969',linetype=2){
+fun.plot.ggplot.vline <- function(g=g,xintercept,col='#696969',linetype=2){
   g <- g + geom_vline(xintercept = xintercept,col = col,linetype = linetype)
   return(g)
 }
 
-fun.plot.ggplot.hline <- function(yintercept,col='#696969',linetype=2){
+fun.plot.ggplot.hline <- function(g=g,yintercept,col='#696969',linetype=2){
   g <- g + geom_hline(yintercept = yintercept,col = col,linetype = linetype)
   return(g)
 }
 
-fun.plot.ggplot.text.repel <- function(data,size=6,load.library=F,col='#696969'){
+fun.plot.ggplot.text.repel <- function(g=g,data,size=6,load.library=F,col='#696969'){
   if(load.library==T){lapply(c('ggrepel','tidyr'),require,character.only = T)}
   gathered.df <- gather(data = data,key = Index,value = Value,colnames(data)[-1],convert = T)
   assign('gathered.df',gathered.df,envir = .GlobalEnv)
@@ -29,13 +29,13 @@ fun.plot.ggplot.text.repel <- function(data,size=6,load.library=F,col='#696969')
   return(g)
 }
 
-fun.plot.ggplot.encircle <- function(data,size=1,expand=0.1,color='red',legend=F){
+fun.plot.ggplot.encircle <- function(g=g,data,size=1,expand=0.1,color='red',legend=F){
   g <- g + geom_encircle(data = data,aes(x = data$x, y = data$y),show.legend = legend,
                          color = color,size = size,expand = expand)
   return(g)
 }
 
-fun.plot.ggplot.mean.median.hline <- function(obj,date.col=1,data.col=2,col='#696969',linetype=2,angle=0,vjust=0,hjust=0,size=4){
+fun.plot.ggplot.mean.median.hline <- function(g=g,obj,date.col=1,data.col=2,col='#696969',linetype=2,angle=0,vjust=0,hjust=0,size=4){
   g <- g + geom_hline(yintercept = mean(obj[,data.col]),col = col,linetype = linetype)
   g <- g + geom_hline(yintercept = median(obj[,data.col]),col = col,linetype = linetype)
   g <- g + annotate('text',x = min(obj[,date.col]),y = mean(obj[,data.col]),label = 'Mean',
