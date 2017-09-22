c3a5552590dd0819b59ce3da07f26ff8fb164028
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-09-22 22:41:11 +0900

    update
    sheettitle,sheetunit修正、colname修正

--- public.code/fun.read.jgb.trade.r
+++ public.code/fun.read.jgb.trade.r
@@ -1,4 +1,4 @@
-fun.read.jgb.trade <- function(load.library = F,path.to.folder = '/Desktop/R_Data_Write/',sheet.unit = '億円'){
+fun.read.jgb.trade <- function(load.library = F,path.to.folder = '/Desktop/R_Data_Write/'){
   # http://www.jsda.or.jp/shiryo/toukei/toushika/tkbk/index.html
   if(load.library==T){lapply(c('XLConnect','Nippon','lubridate'),require,character.only = T)}
   buf.wd <- getwd()
@@ -8,24 +8,25 @@ fun.read.jgb.trade <- function(load.library = F,path.to.folder = '/Desktop/R_Dat
   xls.file <- gsub('.+/([^/]+)','\\1',target.url)
   Sys.sleep(1) # To prevent server overload
   download.file(url = target.url,xls.file,mode = 'wb')
-  sheet.name <- getSheets(loadWorkbook(xls.file))
+  sheet.name <- sapply(getSheets(loadWorkbook(xls.file)),zen2han)
   sheet.title <- vector()
   for(sheet in 1:2){
     tmp0 <- readWorksheetFromFile(xls.file,sheet = sheet,check.names = F,header = F)
     buf0 <- tmp0
-    sheet.title[sheet] <- gsub('(.+)\n.+','\\1',zen2han(paste0(buf0[1,2],':',buf0[3,2])))
-    sheet.unit <- sheet.unit
-    for(rrr in 3:6){
+    sheet.unit <- zen2han(buf0[1,24])
+    sheet.title[sheet] <- zen2han(buf0[1,2])
+    for(rrr in 3:5){
       tmp <- NA
       for(ccc in seq(ncol(buf0))){
         if(!is.na(buf0[rrr,ccc])){
-          tmp <- gsub('\\s','',gsub('([^\n]+)\n.+','\\1',zen2han(buf0[rrr,ccc])))
+          tmp <- gsub('\\s|\\([a-z]+\\)|-','',gsub('([^\n]+)\n.+','\\1',zen2han(buf0[rrr,ccc])),ignore.case = T)
         }
         buf0[rrr,ccc] <- tmp
       }
     }
-    colnames(buf0) <- gsub('内訳:|:NA','',paste0(buf0[3,],':',buf0[4,],':',buf0[5,],':',buf0[6,]))
-    buf1 <- buf0[!is.na(buf0[,1]),]
+    buf0[6,]<-gsub('([^\n]+)\n.+','\\1',sapply(buf0[6,],zen2han))
+    colnames(buf0) <- gsub('内訳:|:NA','',paste0(buf0[3,],':',buf0[4,],':',buf0[5,],':',buf0[6,],sheet.unit))
+    buf1 <- buf0[!is.na(as.numeric(buf0[,1])),]
     date <- as.Date(paste0(gsub('\\.','-',buf1[,1]),'-1'))
     buf2 <-
       data.frame(Date = date,apply(buf1[,-1],2,function(x)as.numeric(gsub(',|－','',x))),
@@ -34,7 +35,8 @@ fun.read.jgb.trade <- function(load.library = F,path.to.folder = '/Desktop/R_Dat
   }
   setwd(buf.wd)
   returnList <-
-    list('sheet.title' = sheet.title,
-         'jgb.trade' = jgb.trade)
+    list('sheet.name' = paste0(sheet.name),
+         'jgb.trade' = jgb.trade,
+         'sheet.title' = sheet.title)
   return(returnList)
 }
