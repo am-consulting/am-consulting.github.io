b5a25e29cff71f8f0b2e00c900045b1cfbb4adc5
am-consulting <amc.mg.drive.8207.sgtr.6754@gmail.com>
2017-10-15 17:12:15 +0900

    table向けjs,css
    table向けjs,css

---
+++ table.components/script.js
@@ -0,0 +1,9 @@
+function MakeNegative() {
+/* Reference Reference http://www.hanselman.com/blog/MakingNegativeNumbersTurnRedUsingCSSAndJavascript.aspx */
+  TDs = document.getElementsByTagName('td');
+  for (var i=0; i<TDs.length; i++) {
+    var temp = TDs[i];
+/*    if (temp.firstChild.nodeValue.indexOf('-') == 0) temp.className = "negative";*/
+    if (temp.firstChild.nodeValue.replace(/(\r\n|\n|\r)/gm,"").indexOf('-') == 0) temp.className = "negative";
+  }
+}
